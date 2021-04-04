ad981be59538b343ef2207fd47894547e3fa8eb0
John <twglhk@gmail.com>
2021-04-04 14:02:42 +0900

    init : Entity Framework Core (.NET ORM)

---
+++ EFCore_sample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EFCore_sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
