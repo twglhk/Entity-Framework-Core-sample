0c454b63ab711236a89189cc2e7e0cbff08f35a8
John <twglhk@gmail.com>
2021-04-15 10:13:26 +0900

    add : data modeling - data annotation sample

---
+++ EFCore_sample/DataModelingSample.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCore_sample
+{
+    [Table("TableName")]  // Set TableName
+    public class GameResult
+    {
+        [MaxLength(32)]       // Set String Size
+        [Column("ColumName")] // Set ColumnName
+        public string UserName { get; set; }
+
+        [NotMapped] // Non DB mapping
+        public string Id { get; set; }
+
+        [Key]   // Set PK (Composite key)
+        [Column(Order = 0)]  // Set Colum Order
+        public int RankingId { get; set; }
+
+        [Key] // Set PK (Composite key)
+        [Column(Order = 1)]
+        [Required] // not null
+        public int SocreId { get; set; }
+    }
+}
